8476fb541b5c9d0a032995ad9164e7b01a319093
Awaizkazi <Owaizkazi789@gmail.com>
2023-05-23 20:18:09 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chat_gpt_flutter_1/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        appBarTheme: AppBarTheme(color: cardColor),
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+//TODO https://www.youtube.com/watch?v=BB59SwaQjrQ
+//  20:53 se dekna baki hai
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,28 @@
+import 'package:chat_gpt_flutter_1/services/assets_manager.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        leading: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Image.asset(AssetsManager.openaiLogo),
+        ),
+        title: Text('ChatGPT'),
+      ),
+      body: Center(
+        child: Image.asset(AssetsManager.openaiLogo),
+      ),
+    );
+  }
+}
