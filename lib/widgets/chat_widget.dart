e0ac0aed4572d005c75131f4a20efa98ff1f5c6e
Awaizkazi <Owaizkazi789@gmail.com>
2023-05-24 20:28:47 +0530

    c

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,10 +1,19 @@
+import 'package:chat_gpt_flutter_1/services/assets_manager.dart';
 import 'package:flutter/material.dart';
 
 class ChatWidget extends StatelessWidget {
   const ChatWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [],);
+    return Column(
+      children: [
+        Row(
+          children: [
+            Image.asset(AssetsManager.imagePath),
+          ],
+        )
+      ],
+    );
   }
 }
